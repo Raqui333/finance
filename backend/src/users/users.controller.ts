1d9bd5663c026e59fa9e569d5b3dd9812e0e57fe
Raqui333 <klebersilva2025@gmail.com>
2025-02-16 18:05:00 -0300

    feat: implement CRUD operations for users in the users controller and service

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { users } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succefully created a user',
+  })
+  createUser(@Body() createUserDTO: Prisma.usersCreateInput) {
+    return this.usersService.create(createUserDTO);
+  }
 
   @Get()
   @ApiOperation({ summary: 'Get all users' })
@@ -15,5 +33,38 @@ export class UsersController {
   })
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get one user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succefully returned a user',
+  })
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update one user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succefully updated a user',
+  })
+  update(
+    @Param('id') id: number,
+    @Body() updateUserDTO: Prisma.usersUpdateInput
+  ) {
+    return this.usersService.update(id, updateUserDTO);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete one user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succefully deleted a user',
+  })
+  delete(@Param('id') id: number) {
+    return this.usersService.delete(id);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,15 +1,72 @@
 import { DatabaseService } from '@/database/database.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { randomBytes, pbkdf2Sync } from 'crypto'; // for hashing the password
 
 @Injectable()
 export class UsersService {
   constructor(private readonly databaseService: DatabaseService) {}
 
+  private hashPassword(password: string) {
+    const salt = randomBytes(16).toString('hex');
+    const hash = pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    return `${hash}.${salt}`;
+  }
+
+  verifyPassword(password: string, hashedPassword: string) {
+    const [originalHash, salt] = hashedPassword.split('.');
+    const hash = pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    return hash === originalHash;
+  }
+
+  async create(createUserDTO: Prisma.usersCreateInput) {
+    try {
+      const { id } = await this.databaseService.users.create({
+        data: {
+          ...createUserDTO,
+
+          // normalize usernane
+          username: createUserDTO.username.toLocaleLowerCase(),
+
+          // hash password
+          password: this.hashPassword(createUserDTO.password),
+
+          balance: 0,
+        },
+      });
+
+      return { message: 'Succefully create a new user', user_id: id };
+    } catch (err) {
+      throw err;
+    }
+  }
+
   async findAll() {
     const users_array = await this.databaseService.users.findMany({
       omit: { password: true },
     });
+
     if (!users_array.length) throw new NotFoundException('No users found');
+
     return users_array;
+  }
+
+  async findOne(id: number) {
+    const user = await this.databaseService.users.findUnique({
+      where: { id },
+      omit: { password: true },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+
+  async update(id: number, updateUserDTO: Prisma.usersUpdateInput) {
+    // todo
+  }
+
+  async delete(id: number) {
+    // todo
   }
 }
