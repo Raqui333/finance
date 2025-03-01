c559a791214b6cbcd7de8940392d665012c1018a
Raqui333 <klebersilva2025@gmail.com>
2025-03-01 19:08:28 -0300

    feat: add user data fetching to PerformanceCard component

--- frontend/src/components/PerformanceCard.tsx
+++ frontend/src/components/PerformanceCard.tsx
@@ -1,50 +1,66 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useAppSelector } from '@/redux/hooks';
 import formatCurrency from '@/utils/currency-formatter';
 
-import { alpha, Box, Paper, Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 import { SparkLineChart } from '@mui/x-charts';
+import { getEntriesFromUser } from '@/utils/actions';
 
 interface PerformanceCardProps {
   name: string;
+  userId: number;
 }
 
 function calculateDelta(start_value: number, last_value: number) {
-  if (!start_value || start_value === 0) return 0;
+  if (isNaN(start_value) || start_value === 0) return 0;
 
   return ((last_value - start_value) * 100) / start_value;
 }
 
-export default function PerformanceCard({ name }: PerformanceCardProps) {
-  const currency = useAppSelector((state) => state.currency.value);
-  const entries = useAppSelector((state) => state.user.entries);
+function getSequencialSumArray(data: UserEntry[]) {
+  // get the sum of the prices of the entries sequencially
+  // eg: [1, 2, 3] => [1, 3, 6]
+  let sum = 0;
+  return data.map((row) => {
+    sum += row.price;
+    return sum;
+  });
+}
 
-  const data = useMemo(() => {
-    let sum = 0;
-    return entries.map((row) => {
-      sum += row.price;
-      return sum;
-    });
-  }, [entries]);
+const mainBoxStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  '&:hover': {
+    filter: 'brightness(1.2)',
+  },
+};
+
+export default function PerformanceCard({
+  name,
+  userId,
+}: PerformanceCardProps) {
+  const currency = useAppSelector((state) => state.currency.value);
+
+  const [entries, setEntries] = useState<UserEntry[]>([]);
+
+  useEffect(() => {
+    getEntriesFromUser(userId)
+      .then(async (res) => {
+        setEntries(res);
+      })
+      .catch((err) => console.error(err));
+  }, [userId]);
+
+  const data = useMemo(() => getSequencialSumArray(entries), [entries]);
 
   const total = data.length > 0 ? data[data.length - 1] : 0;
   const delta = calculateDelta(data[0], total);
 
   return (
-    <Box
-      component={Paper}
-      sx={(theme) => ({
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        '&:hover': {
-          backgroundColor: alpha(theme.palette.primary.main, 0.01),
-        },
-        transition: 'background-color 0.3s ease',
-      })}
-    >
+    <Box component={Paper} sx={{ ...mainBoxStyle }}>
       <Box sx={{ alignSelf: 'flex-start' }}>
         <Typography>{name}</Typography>
         <Typography
@@ -65,7 +81,7 @@ export default function PerformanceCard({ name }: PerformanceCardProps) {
         curve="catmullRom"
         showHighlight={true}
         showTooltip={true}
-        valueFormatter={(value) => formatCurrency(value, currency)}
+        valueFormatter={(value) => formatCurrency(value as number, currency)}
       />
     </Box>
   );
