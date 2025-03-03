74038733c2b0b66575d08c8a9a6131dbec3388f6
Raqui333 <klebersilva2025@gmail.com>
2025-03-03 18:27:21 -0300

    feat: update user slice to remove balance and modify entry management

--- frontend/src/redux/features/user/userSlice.ts
+++ frontend/src/redux/features/user/userSlice.ts
@@ -2,13 +2,11 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 type UserState = {
   name: string;
-  balance: number;
   entries: UserEntry[];
 };
 
 const initialState: UserState = {
-  name: 'RAQUI',
-  balance: 0,
+  name: 'N/A',
   entries: [],
 };
 
@@ -19,29 +17,20 @@ const userSlice = createSlice({
     setName(state, action: PayloadAction<string>) {
       state.name = action.payload;
     },
-    setBalance(state, action: PayloadAction<number>) {
-      state.balance += action.payload;
+    setEntry(state, action: PayloadAction<UserEntry[]>) {
+      state.entries = action.payload;
     },
     addEntry(state, action: PayloadAction<UserEntry>) {
-      const last_entry = state.entries.at(-1) ?? null;
-      const last_id = last_entry?.id ?? -1;
-      // adds a new object with a sequential ID
-      state.entries.push({
-        ...action.payload,
-        id: last_id + 1,
-        createdAt: Date.now(),
-      });
+      state.entries.push(action.payload);
     },
     removeEntry(state, action: PayloadAction<number | undefined>) {
       const index_to_remove = state.entries.findIndex(
         (entry) => entry.id === action.payload
       );
-
-      state.balance -= state.entries[index_to_remove].price;
       state.entries.splice(index_to_remove, 1);
     },
   },
 });
 
-export const { setName, setBalance, addEntry, removeEntry } = userSlice.actions;
+export const { setName, setEntry, addEntry, removeEntry } = userSlice.actions;
 export default userSlice.reducer;
