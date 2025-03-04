690a84f511db3d93a9d84b4c07ae8622813d5364
Raqui333 <klebersilva2025@gmail.com>
2025-03-04 16:17:52 -0300

    feat: enhance fetchAPI to include token validation for protected routes and add login function

--- frontend/src/utils/actions.ts
+++ frontend/src/utils/actions.ts
@@ -4,11 +4,14 @@ import { cookies } from 'next/headers';
 
 const API = process.env.API_URL || 'http://localhost:3000';
 
+const public_routes = ['/auth/login', '/auth/register'];
+
 async function fetchAPI(endpoint: string, options: RequestInit = {}) {
   try {
     const token = await cookies().then((res) => res.get('access_token')?.value);
 
-    if (!token) throw new Error('No Token Request');
+    if (!public_routes.includes(endpoint) && !token)
+      throw new Error('No Token Request');
 
     const resp = await fetch(`${API}${endpoint}`, {
       cache: 'no-store',
@@ -30,10 +33,10 @@ async function fetchAPI(endpoint: string, options: RequestInit = {}) {
 }
 
 export async function getUserProfile() {
-  return await fetchAPI(`/users`);
+  return await fetchAPI('/users');
 }
 export async function getEntriesFromUser() {
-  return await fetchAPI(`/assets`);
+  return await fetchAPI('/assets');
 }
 
 export async function removeUserEntry(id: number) {
@@ -41,7 +44,7 @@ export async function removeUserEntry(id: number) {
 }
 
 export async function createUserEntry(entry: UserEntryForPost) {
-  return await fetchAPI(`/assets`, {
+  return await fetchAPI('/assets', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -49,4 +52,18 @@ export async function createUserEntry(entry: UserEntryForPost) {
       date: new Date(entry.date).toISOString(),
     }),
   });
+}
+
+export async function login(username: string, password: string) {
+  const response = await fetchAPI('/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password }),
+  });
+
+  await cookies().then((res) =>
+    res.set('access_token', response.access_token, {
+      path: '/',
+    })
+  );
 }
