b4eb0550f295f68291394ac8e8a228bb1e6e7d5d
Raqui333 <klebersilva2025@gmail.com>
2025-03-03 21:17:10 -0300

    feat: implement signUp method for user registration and refactor signIn to use UsersService

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,20 +1,19 @@
-import { DatabaseService } from '@/database/database.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PasswordService } from './password.service';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '@/users/users.service';
+import { RegisterDTO } from './DTOs/register.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly databaseService: DatabaseService,
     private readonly passwordService: PasswordService,
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService
   ) {}
 
   async signIn(username: string, pass: string) {
-    const user = await this.databaseService.users.findUnique({
-      where: { username },
-    });
+    const user = await this.usersService.findOneByUsername(username);
 
     if (!user || !this.passwordService.verifyPassword(pass, user.password))
       throw new UnauthorizedException();
@@ -24,5 +23,14 @@ export class AuthService {
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async signUp(registerDTO: RegisterDTO) {
+    const password = this.passwordService.hashPassword(registerDTO.password);
+
+    return this.usersService.create({
+      ...registerDTO,
+      password,
+    });
   }
 }
