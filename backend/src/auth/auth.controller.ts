745afef17e5a3179aecff6470b7aed56fe60f060
Raqui333 <klebersilva2025@gmail.com>
2025-03-03 21:17:19 -0300

    feat: enhance AuthController with Swagger documentation and public access for login and registration

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,33 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from '@/decorators/public.decorator';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { SignInDTO } from './DTOs/login.dto';
+import { RegisterDTO } from './DTOs/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succefully log in',
+  })
+  signIn(@Body() signInDTO: SignInDTO) {
+    return this.authService.signIn(signInDTO.username, signInDTO.password);
+  }
+
+  @Public()
+  @Post('register')
+  @ApiOperation({ summary: 'Register an user' })
+  @ApiResponse({
+    status: 200,
+    description: 'Succefully registered an user',
+  })
+  signUp(@Body() RegisterDTO: RegisterDTO) {
+    return this.authService.signUp(RegisterDTO);
   }
 }
