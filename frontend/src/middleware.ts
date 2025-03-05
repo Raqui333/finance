8c105124b5560f6d9ace19fc21e350c6e442831e
Raqui333 <klebersilva2025@gmail.com>
2025-03-05 16:49:04 -0300

    feat: implement token validation in middleware for enhanced route protection

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,17 +1,33 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
+const API = process.env.API_URL || 'http://localhost:3000';
+
 const public_routes = ['/auth/login', '/auth/register'];
 
-export function middleware(req: NextRequest) {
+async function validadeToken(token: string | undefined) {
+  if (!token) return false;
+
+  const resp = await fetch(`${API}/auth/validate-token`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (resp.status === 401) return false;
+
+  return true;
+}
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get('access_token')?.value;
   const { pathname } = req.nextUrl;
 
   const isPrivateRoute = !public_routes.includes(pathname);
+  const isValidToken = await validadeToken(token);
 
-  if (isPrivateRoute && !req.cookies.has('access_token'))
+  if (isPrivateRoute && !isValidToken)
     return NextResponse.redirect(new URL('/auth/login', req.url));
 
-  if (pathname.startsWith('/auth') && req.cookies.has('access_token'))
+  if (pathname.startsWith('/auth') && isValidToken)
     return NextResponse.redirect(new URL('/', req.url));
 
   return NextResponse.next();
