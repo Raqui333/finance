dc6dc49326e4e622f5abb7c59c48b52280a8cd33
raqui333 <klebersilva2025@gmail.com>
2025-07-24 06:05:28 -0300

    fix login

--- frontend/src/app/api/auth/login/route.ts
+++ frontend/src/app/api/auth/login/route.ts
@@ -3,16 +3,16 @@ export async function POST(request: Request) {
   const { username, password } = body;
 
   if (username !== 'admin' && password !== 'admin')
-    return new Response(
-      JSON.stringify({ message: 'Unauthorized', statusCode: 401 }),
-      {
-        status: 401,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
+    return new Response(JSON.stringify({ access_token: 'DEMO_TOKEN' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  return new Response(JSON.stringify({ access_token: 'DEMO_TOKEN' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return new Response(
+    JSON.stringify({ message: 'Unauthorized', statusCode: 401 }),
+    {
+      status: 401,
+      headers: { 'Content-Type': 'application/json' },
+    }
+  );
 }
